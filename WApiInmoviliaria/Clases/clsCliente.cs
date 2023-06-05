7f099c515f1ff0c587175444056d8fd884bf48dd
Lukas-jb <92315994+Lukas-jb@users.noreply.github.com>
2023-06-04 21:52:47 -0500

    Se ajusta, queda funcional el api/consultar cliente

--- WApiInmoviliaria/Clases/clsCliente.cs
+++ WApiInmoviliaria/Clases/clsCliente.cs
@@ -2,10 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using WApiInmoviliaria.Models;
+
 
 namespace WApiInmoviliaria.Clases
 {
     public class clsCliente
     {
+        DBInmobiliariaEntities dbsuper = new DBInmobiliariaEntities();
+
+        public Cliente Consultar(int idCliente)
+        {
+            return dbsuper.Clientes.FirstOrDefault(p => p.id_cliente == idCliente);
+        }
     }
 }
